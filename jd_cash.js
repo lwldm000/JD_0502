526dc94651cb20c48408a2e1b18f686c7a330008
lwldm000 <80460446+lwldm000@users.noreply.github.com>
2021-05-27 08:18:13 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -26,7 +26,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let allMessage = "";
+//let allMessage = "";
 const randomCount = $.isNode() ? 20 : 5;
 let t = +new Date()
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
